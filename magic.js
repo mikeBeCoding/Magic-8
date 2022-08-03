340460dbf73ec1a21f35c8c1cf819d4dec6d44f2
mikeBeCoding <74873623+mikeBeCoding@users.noreply.github.com>
2022-08-02 21:15:07 -0700

    Cleaned up unused code...
    Also, I chose to hide the question container during the shake.

--- magic.js
+++ magic.js
@@ -17,21 +17,19 @@ const hide = (Id) => {
    eightBall = 'It is certain';
    answer.innerText = eightBall;
    console.log(eightBall);
-   document.write(answer);
+
    break;
 
    case 1:
    eightBall = 'It is decidedly so';
    answer.innerText = eightBall;
    console.log(eightBall)
-   document.write(answer);
    break;
 
    case 2:
    eightBall = 'Reply hazy, try again';
    answer.innerText = eightBall;
    console.log(eightBall);
-   document.write(answer);
    break;
    case 3:
 
@@ -45,105 +43,90 @@ const hide = (Id) => {
    eightBall = 'Do not count on it';
    answer.innerText = eightBall;
    console.log(eightBall);
-   document.write(answer);
    break;
 
    case 5:
    eightBall = 'My sources say no';
    answer.innerText = eightBall;
    console.log(eightBall);
-   document.write(answer);
    break;
 
    case 6:
    eightBall = 'Outlook not so good';
    answer.innerText = eightBall;
    console.log(eightBall);
-   document.write(answer);
    break;
 
    case 7:
    eightBall = 'Signs point to yes';
    answer.innerText = eightBall;
    console.log(eightBall);
-   document.write(answer);
    break;
    
    case 8:
    eightBall = 'Ehh';
    answer.innerText = eightBall;
    console.log(eightBall);
-   document.write(answer);
    break;
 
    case 9:
    eightBall = "Don't quit your day job";
    answer.innerText = eightBall;
    console.log(eightBall);
-   document.write(answer);
    break;
 
    case 10:
    eightBall = 'Ask again later';
    answer.innerText = eightBall;
    console.log(eightBall);
-   document.write(answer);
    break;
 
    case 11:
    eightBall = 'It is possible';
    answer.innerText = eightBall;
    console.log(eightBall);
-   document.write(answer);
    break;
 
    case 12:
-   eightBall = "Yes, yes, and yes";
+   eightBall = "Yes, Yes, and Yes";
    answer.innerText = eightBall;
    console.log(eightBall);
-   document.write(answer);
    break;    
     
    case 13:
    eightBall = "Take the risk or lose the chance";
    answer.innerText = eightBall;
    console.log(eightBall);
-   document.write(answer);
    break;
     
    case 14:
    eightBall = "If nothing goes right, go left";
    answer.innerText = eightBall;
    console.log(eightBall);
-   document.write(answer);
    break;
     
    case 15:
    eightBall = "Good things happen to those who hustle";
    answer.innerText = eightBall;
    console.log(eightBall);
-   document.write(answer);
    break;   
    
    case 16:
    eightBall = "Dream big. Pray bigger";
    answer.innerText = eightBall;
    console.log(eightBall);
-   document.write(answer);
    break;
 
    case 17:
    eightBall = "If you want it, work for it";
    answer.innerText = eightBall;
    console.log(eightBall);
-   document.write(answer);
    break;
 
    case 18:
    eightBall = "NO... Yes! Works every time";
    answer.innerText = eightBall;
    console.log(eightBall);
-   document.write(answer);
    break;
      
    default: 
@@ -163,14 +146,19 @@ const magic = () =>  {
 
 
         if (question.value.length >= 5){
-            // console.log(i)
+
             question.value = 'it is working its magic';
             outerShell.style.animationPlayState = 'running';
             mainContainer.style.animationPlayState = 'running';
 
             setTimeout(() => {
+                hide('question-container');
+            },100);
+            
+            setTimeout(() => {
                 hide('bottom-8-white');
-            },500);
+
+            },2000);
 
             setTimeout( ()=> {
                 show('answer-container');
@@ -180,10 +168,9 @@ const magic = () =>  {
 
             setTimeout(function (){
                 hide('answer-container');
-                question.value = '';
                 location.reload();
                 return false;
-                }, 7000);
+                }, 6000);
         }
 
     
